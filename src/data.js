94dd91d09f058e2ef78e2fc6acd0efd7c1793f8b
racray <rayedkhan007@gmail.com>
2022-01-25 20:27:46 +0530

    hope

---
+++ src/data.js
@@ -0,0 +1,45 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://cdn.shopify.com/s/files/1/0549/2349/3541/products/d5e47a69b6e30edf279c926d141b80de_800x.jpg?v=1622710686",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://i.ibb.co/DG69bQ4/2.png",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://i.ibb.co/cXFnLLV/3.png",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "SHIRT STYLE!",
+      cat:"woman"
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "LOUNGEWEAR LOVE",
+      cat:"coat"
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      title: "WOMAN SKIRTS",
+      cat:"skirt"
+    },
+  ];
+
---
+++ src/redux/cartRedux.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    products: [],
+    quantity: 0,
+    total: 0,
+  },
+  reducers: {
+    addProduct: (state, action) => {
+
+      state.quantity += 1;
+      state.products.push(action.payload);
+      state.total += action.payload.price * action.payload.quantity;
+    },
+    removeQuantity: (state, action) => {
+      state.products = action.payload.products
+      state.quantity = state.quantity > 0 ? state.quantity-1 : 0
+      if(state.total!==0)
+      state.total -= action.payload.price * action.payload.quantityid;
+      else{
+        state.total = 0;
+      }
+    }
+  },
+});
+
+export const { addProduct,removeQuantity} = cartSlice.actions;
+export default cartSlice.reducer;
